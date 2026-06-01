641115512085a434b2a334dbc4c840a8af4cd4fd
meowdev5s <letuanhoi17092005@gmail.com>
2026-06-01 18:01:00 +0700

    update path to db

---
+++ Nhom6_QLThietBi_API/Nhom6_QLThietBi_API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Nhom6_QLThietBi_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor nhận cấu hình (chuỗi kết nối) từ Program.cs truyền vào
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Sau này khai báo các bảng dữ liệu ở đây, ví dụ:
+        // public DbSet<ThietBi> ThietBis { get; set; }
+    }
+}
